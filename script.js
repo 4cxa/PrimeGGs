3a88e446e01d28dbaa7b5c850b4ebd8cb593a1cf
4cxa <127905588+4cxa@users.noreply.github.com>
2024-07-10 12:18:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
     setTimeout(function() {
         window.location.href = "https://discord.gg/85gUuzqhDy";
-    }, 3000); // Redirect after 3 seconds
+    }, 1000); // Redirect after 3 seconds
 };
